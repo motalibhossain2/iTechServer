28476e7e8d83f646adab1ac7ee48281a23623f60
motalibhossain2 <113284968+motalibhossain2@users.noreply.github.com>
2024-05-13 23:36:15 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index file
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db_videos");
+const router = jsonServer.router("db_blogs");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
